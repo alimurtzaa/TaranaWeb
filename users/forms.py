6857fbc816213c59481a2c3d601e7758b9115e41
Murtza ali <alimurtzaa2004@gmail.com>
2025-01-29 01:02:46 +0530

    adding placeholder

--- users/forms.py
+++ users/forms.py
@@ -12,5 +12,8 @@ class ProfileForm(ModelForm):
         }
         widgets = {
             'image': forms.FileInput(),
-            'bio': forms.Textarea(attrs={'rows':3})
+            'bio': forms.Textarea(attrs={'rows':3, 'placeholder':'Tell us about yourself'}),
+            'full_name': forms.TextInput(attrs={'placeholder':'Name'}),
+            'location': forms.TextInput(attrs={'placeholder':'Location'}),
+            'email': forms.EmailInput(attrs={'placeholder':'Email'}),
         }
